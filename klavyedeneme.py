79d3c8dc60c3cabed449890fc88dd198074cf8ea
yoelkastro <32455293+yoelkastro@users.noreply.github.com>
2018-02-24 11:35:25 +0000

    Update klavyedeneme.py

--- klavyedeneme.py
+++ klavyedeneme.py
@@ -1,19 +1,12 @@
-import pygame
 from pololu_drv8835_rpi import motors
 import math
 import sys
-import picamera
-import termios
-import tty
-pygame.init()
+import keyboard
 
-
-##camera = picamera.PiCamera()
 lx = 0
 ly = 0
 i = 0
 
-##camera.resolution = (1920, 1080)
 
 def getValue(x, y, t):
     if(t):
@@ -29,29 +22,21 @@ def getValue(x, y, t):
 
 while True:
     record = False
-    ch = ""
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
 
-    if(ch == "w"):
-        lx = -1
+    ly = 0
+    if(keyboard.is_pressed('w')):
+        print("forward")
         ly = -1
-    elif(ch == "s"):
-        lx = 1
+    elif(keyboard.is_pressed('s')):
+        print("backward")
         ly = 1
-    elif(ch == "a"):
-        lx = 1
-        ly = -1 
-    elif(ch == "d"):
+
+    lx = 0
+    if(keyboard.is_pressed('a')):
+        print("right")
         lx = -1
-        ly = 1
-    else:
-        lx = 0
-        ly = 0
-      
+    elif(keyboard.is_pressed('d')):
+        print("left")
+        lx = 1
+     
     motors.setSpeeds(getValue(lx, ly, True), getValue(lx, ly, False))
