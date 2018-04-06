c0a22ab0c259106c9aaeeed4423c988abd08a060
yoelkastro <32455293+yoelkastro@users.noreply.github.com>
2018-04-06 14:36:06 +0300

    Update SpeedTest.py

--- SpeedTest.py
+++ SpeedTest.py
@@ -20,10 +20,7 @@ camera.framerate = 100
 rawCapture = PiRGBArray(camera, size=(640, 480))
 motors.setSpeeds(0, 0)
 time.sleep(0.1)
-rainbow_lower = [[4,31,120],[31,76,4],[76,31,4]]
-#[30,104,104],[31,76,4],[76,31,4]
-rainbow_upper = [[70,90,210],[90,210,70],[190,90,70]]
-#[50,240,241],[90,210,70],[190,90,70]]
+isMove = 1
 
 while True:
         for e in pygame.event.get():
@@ -31,16 +28,15 @@ while True:
                         if(e.button == 12):
                                 sys.exit()
                         elif(e.button == 14):
-                                color = 0
                                 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    
+                          
                                         image = frame.array
 
                                         blur = cv2.blur(image, (3,3))
 
                                         
-                                        lower = np.array(rainbow_lower[color],dtype="uint8")
-                                        upper = np.array(rainbow_upper[color], dtype="uint8")
+                                        lower = np.array([0, 0, 0], dtype="uint8")
+                                        upper = np.array([20, 20, 20], dtype="uint8") #white
         
                                         thresh = cv2.inRange(blur, lower, upper)
         
@@ -57,23 +53,15 @@ while True:
                                         cx,cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
                                         rawCapture.truncate(0)
 
-                                        if(cx < 380 and cx > 260):
-                                            print('yes')
-                                            motors.setSpeeds(400, 420)
-                                            time.sleep(0.5)
-                                            motors.setSpeeds(-420, -400)
-                                            time.sleep(0.5)
-                                            motors.setSpeeds(0, 0)
-                                            color = color + 1
-                                            if(color == 4):
-                                                    break
-                                                
-                                        else:
-                                            motors.motor1.setSpeed(175)
-                                            motors.motor2.setSpeed(-175)
+                                        motors.setSpeeds(
+                                               ((1.125 * cx + 120) * fabs(floor(cx / 320) - 1)) + (480 * fabs(floor(cx / 320))) * isMove, #left
+                                               ((- 1.125 * cx + 840) * fabs(floor(cx / 320))) + (480 * fabs(floor(cx / 320) - 1)) * isMove #right
+                                        )
                                         for e in pygame.event.get():
                                                 if(e.type == pygame.JOYBUTTONDOWN):
                                                         if(e.button == 12):
-                                                                sys.exit()
+                                                                isMove = 0
+                                                        elif(e.button = 14):
+                                                                isMove = 1
         
 
