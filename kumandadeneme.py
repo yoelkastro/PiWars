a0788358958e9e8cee3e163bd1f340609aca4336
yoelkastro <32455293+yoelkastro@users.noreply.github.com>
2018-04-06 13:58:42 +0300

    Added a start for other challenges

--- kumandadeneme.py
+++ kumandadeneme.py
@@ -13,7 +13,14 @@ lx = 0
 ly = 0
 i = 0
 
+GPIO.setup(18, GPIO.OUT)
+golfServo = GPIO.PWM(18, 180)
+golfServo.start(5)
+golfAngle = 0
+
 camera.resolution = (1920, 1080)
+
+def shoot():
 
 def getValue(x, y, t):
     if(t):
@@ -28,7 +35,7 @@ def getValue(x, y, t):
             return (int)((-y - x) * 480)
 
 while True:
-    record = False
+    buttons = []
     for e in pygame.event.get():
         if(e.type == pygame.JOYAXISMOTION):
             if(e.axis == 0):
@@ -36,21 +43,21 @@ while True:
             elif(e.axis == 1):
                 ly = e.value
         if(e.type == pygame.JOYBUTTONDOWN):
-            if(e.button == 14):
-                string = 'fizliresim'
-                string += str(i)
-                string += '.jpg'
-                camera.capture(string)
-                i = i + 1
-            elif(e.button == 12):
-                string = 'video.h264'
-                if record:
-                    camera.stop_recording()
-                    record = False
-                else:
-                    camera.start_recording(string)
-                    record = True
-            else:
-                sys.exit()
+            
+            buttons.append(e.button)
+            
+            #else:
+                #sys.exit()
         
     motors.setSpeeds(getValue(lx, ly, True), getValue(lx, ly, False))
+    
+    if 14 in buttons:
+        shoot()
+    if R2 in buttons:
+        if golfAngle > 0:
+            golfAngle -= 10
+            golfServo.ChangeDutyCycle(golfAngle)
+    if L2 in buttons:
+        if golfAngle < 180:
+            golfAngle += 10
+            golfServo.ChangeDutyCycle(golfAngle)
